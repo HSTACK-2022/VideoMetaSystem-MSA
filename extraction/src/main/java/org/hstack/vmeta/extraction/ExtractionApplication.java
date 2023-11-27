68ee268586ebc8b419db3b92ef2ba944593f41a8
yeondelight <hihi5v@hansung.ac.kr>
2023-11-27 21:31:02 +0900

    initial commit

---
+++ extraction/src/main/java/org/hstack/vmeta/extraction/ExtractionApplication.java
@@ -0,0 +1,13 @@
+package org.hstack.vmeta.extraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractionApplication.class, args);
+	}
+
+}
---
+++ extraction/src/test/java/org/hstack/vmeta/extraction/ExtractionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hstack.vmeta.extraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
